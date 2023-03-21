28a8f843aa2c71830bbb536d0a96906fa6a409e0
VictorDavidRodriguezSalinas <57460380+VictorDavidRodriguezSalinas@users.noreply.github.com>
2023-03-20 21:20:00 -0300

    copia4

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $clients=Client::all();
+        return response()->json($clients);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+       $client = new Client;
+       $client->name=$request->name;
+       $client->email=$request->email;
+       $client->phone=$request->phone;
+       $client->address=$request->address;
+       $client->save();
+        $data=[
+            'message'=> 'Registrado correctamente',
+            'cliente'=> $client
+        ];
+       return response()->json($data);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Client $client)
+    {
+        //
+        return response()->json($client);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Client $client)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Client $client)
+    {
+        //
+        $client->name=$request->name;
+        $client->email=$request->email;
+        $client->phone=$request->phone;
+        $client->address=$request->address;
+        $client->save();
+        $data=[
+            'message'=> 'Actualizado correctamente',
+            'cliente'=> $client
+        ];
+       return response()->json($data);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Client $client)
+    {
+        //
+        $client->delete();
+        $data=[
+            'message'=> 'Actualizado correctamente',
+            'cliente'=> $client
+        ];
+       return response()->json($data);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/clients','App\Http\Controllers\ClientController@index');
+Route::post('/clients','App\Http\Controllers\ClientController@store');
+Route::get('/clients/{client}','App\Http\Controllers\ClientController@show');
+Route::put('/clients/{id}','App\Http\Controllers\ClientController@update');
+Route::delete('/clients/{client}','App\Http\Controllers\ClientController@destroy');
+
